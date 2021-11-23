c8bbdb801acf5d989396ae7954919e0b1928623f
Bart <ibrahimifatbardh70@gmail.com>
2021-11-23 13:24:06 -0600

    commit tree 8cf530e1dd05e70849115da42c41a0d2735c93bb

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
-}
+// Your code here
+function saturdayFun(activity = `roller-skate` ) {
+  return `This Saturday, I want to ${activity}!`    
+} 
 
-let mondayWork = function(activity="go to the office") {
+function mondayWork(activity = `go to the office`) {
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
-  }
+
+function wrapAdjective(flair = `*`) {
+  return function (adjective = `special`) {
+      return `You are ${flair}${adjective}${flair}!`;
+  };
 }
+
+// wrapAdjective(`*`,)
